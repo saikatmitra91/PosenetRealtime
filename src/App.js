ef1d4c97846695ed6fc547f7cb13df1160ace79f
raviteja83 <ravi.theja@100ms.live>
2022-11-13 22:15:51 +0530

    feat: add timers

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
+import JSConfetti from "js-confetti";
 import { drawKeypoints, drawSkeleton } from "./utilities";
 import { poseSimilarity } from "posenet-similarity";
 import { useCounter } from "./useCounter";
+import { images } from "./constants";
 
+const jsConfetti = new JSConfetti();
+let requestId;
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const imgRef = useRef(null);
-  const posenetRef = useRef(false);
+  const posenetRef = useRef(null);
   const [distance, setDistance] = useState(0);
   const [maxDistance, setMaxDistance] = useState(distance);
   const { count: startCounter, start } = useCounter();
   const { count: breakCounter, start: startBreakCounter } = useCounter();
+  const [currentImage, setCurrentImage] = useState(0);
+  const [success, setSuccess] = useState(new Array(images.length).fill(false));
 
   /**
    * @param {posenet.PoseNet} net
    */
-  const detect = useCallback(async (net) => {
+  const detect = useCallback(async () => {
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
+      webcamRef.current.video.readyState === 4 &&
+      posenetRef.current
     ) {
       // Get Video Properties
       const video = webcamRef.current.video;
@@ -33,30 +40,26 @@ function App() {
       // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
+      const net = posenetRef.current;
 
       // Make Detections
-      const expectedPose = await net.estimateSinglePose(imgRef.current);
       const pose = await net.estimateSinglePose(video);
+      const expectedPose = await net.estimateSinglePose(imgRef.current);
       const data = poseSimilarity(expectedPose, pose, {
         strategy: "cosineSimilarity",
       });
       setDistance(data);
       drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
-      window.requestAnimationFrame(() => detect(net));
+      requestId = window.requestAnimationFrame(() => detect());
     }
   }, []);
   //  Load posenet
   const runPosenet = useCallback(async () => {
-    posenetRef.current = true;
-    const net = await posenet.load({
+    posenetRef.current = await posenet.load({
       inputResolution: { width: 640, height: 480 },
       scale: 0.8,
     });
-
-    //
-    // setInterval(() => {
-    window.requestAnimationFrame(() => detect(net));
-    // }, 1000);
+    window.requestAnimationFrame(detect);
   }, [detect]);
 
   const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
@@ -79,36 +82,66 @@ function App() {
     }
   }, [distance, maxDistance]);
 
+  // once the start counter ends or scores exceeds reset and start break of 5secs
+  useEffect(() => {
+    if (startCounter === 0 || maxDistance > 0.99) {
+      setSuccess((value) => {
+        value[currentImage] = maxDistance > 0.99;
+        return value;
+      });
+      setMaxDistance(0);
+      cancelAnimationFrame(requestId);
+      startBreakCounter(5);
+    }
+  }, [maxDistance, startCounter, currentImage]); //eslint-disable-line
+
+  // when break counter resets, move to next image and start again
+  useEffect(() => {
+    if (breakCounter === 0 && currentImage < images.length - 1) {
+      setCurrentImage(currentImage + 1);
+      detect();
+      start(10);
+    }
+  }, [detect, breakCounter, currentImage]); //eslint-disable-line
+
+  // show the success confetti
+  useEffect(() => {
+    if (success.every((value) => value)) {
+      jsConfetti.addConfetti({ emojis: ["✨"] });
+      setSuccess((value) => value.map((val) => false));
+    }
+  }, [success]);
+
   return (
-    <div className="App">
-      <div className="centered counter-container">
-        {startCounter !== -1 ? (
-          <div className="counter">{startCounter}</div>
-        ) : (
-          <button onClick={() => start(10)}>Start Posing</button>
-        )}
+    <div className="container">
+      <div className="centered">
+        <Webcam ref={webcamRef} />
+        <canvas className="canvas" ref={canvasRef} />
       </div>
-      <header
-        className="App-header"
+      <button
+        onClick={() => {
+          start(10);
+        }}
+        disabled={startCounter > -1}
+      >
+        {startCounter > 0 ? startCounter : "Start"}
+      </button>
+      <div
+        className="image-score"
         style={{
-          visibility:
-            startCounter === 0 || breakCounter === 0 ? "visible" : "hidden",
+          visibility: startCounter > -1 ? "visible" : "hidden",
         }}
       >
-        <Webcam ref={webcamRef} className="centered" />
-        <canvas ref={canvasRef} className="centered" />
-        <div className="image-score">
-          <img
-            ref={imgRef}
-            alt="pose"
-            crossOrigin="anonymous"
-            src="https://ucarecdn.com/fd629ccd-c734-4b1d-9659-4381458e0857/-/preview/-/quality/smart/-/format/auto/"
-            id="pose-match"
-            style={{ width: "100%" }}
-          />
-          <h1>{maxDistance.toFixed(2)}</h1>
-        </div>
-      </header>
+        <img
+          ref={imgRef}
+          alt="pose"
+          crossOrigin="anonymous"
+          src={images[currentImage]}
+          id="pose-match"
+          style={{ width: "100%" }}
+        />
+        <h1>{maxDistance.toFixed(2)}</h1>
+      </div>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const images = [
+  "https://ucarecdn.com/26d379cb-ccdb-4521-9403-8c203654134f/-/preview/-/quality/smart/-/format/auto/",
+  "https://ucarecdn.com/fd629ccd-c734-4b1d-9659-4381458e0857/-/preview/-/quality/smart/-/format/auto/",
+];
