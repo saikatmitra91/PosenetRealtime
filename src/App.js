d3052c6d63cc7a375f53dc9057b371054692adfb
Saikat Mitra <saikatmitra91@gmail.com>
2022-11-13 18:00:59 +0530

    init camaera selection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
@@ -10,21 +10,25 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const imgRef = useRef(null);
-  const posenetRef = useRef(false)
+  const isPosnetInit = useRef(false);
+  const posenetRef = useRef(null);
+  const currentDetectionIntervalId = useRef(undefined);
   const [distance, setDistance] = useState(0);
-  const [maxDistance, setMaxDistance] = useState(distance)
+  const [maxDistance, setMaxDistance] = useState(distance);
+  const [ devices, setDevices ] = useState([]);
+  const [ device, setDevice ] = useState("");
 
   //  Load posenet
   const runPosenet = async () => {
-    posenetRef.current = true
-    const net = await posenet.load({
+    isPosnetInit.current = true
+    posenetRef.current = await posenet.load({
       inputResolution: { width: 640, height: 480 },
       scale: 0.8,
     });
     
     //
-    setInterval(() => {
-      detect(net);
+    currentDetectionIntervalId.current = setInterval(() => {
+      detect(posenetRef.current);
     }, 100);
   };
 
@@ -53,7 +57,7 @@ function App() {
         strategy: 'cosineSimilarity'
       })
       setDistance(data)
-      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+      // drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
     }
   };
 
@@ -66,7 +70,7 @@ function App() {
   };
 
   useEffect(() => {
-    if (!posenetRef.current){
+    if (!isPosnetInit.current) {
       runPosenet();
     }
   })
@@ -77,11 +81,25 @@ function App() {
     }
   }, [distance, maxDistance])
 
+  useEffect(() => {
+      navigator
+        .mediaDevices
+        .enumerateDevices()
+        .then(mediaDevices=> {
+          const devices = mediaDevices.filter(({ kind }) => kind === "videoinput")
+          setDevices(devices)
+          setDevice(devices[0])
+        });
+    },
+    []
+  );
+
   return (
     <div className="App">
       <header className="App-header">
           <Webcam
             ref={webcamRef}
+            videoConstraints={{ deviceId: device.deviceId }}
             style={{
               position: "absolute",
               marginLeft: "auto",
@@ -111,6 +129,21 @@ function App() {
         <div className="image-score">
           <img ref={imgRef} crossOrigin="anonymous" src="https://ucarecdn.com/cae252d0-7c80-4523-834c-a0d3e053796f/-/preview/-/quality/smart/-/format/auto/" id="pose-match" style={{ width: "100%"}}/>
           <h1>{maxDistance.toFixed(2)}</h1>
+          <select value={device.deviceId} onChange={(e) => { 
+            const [device] = devices.filter(device => device.label === e.target.value)
+            if(posenetRef.current) {
+              posenetRef.current.dispose()
+            }
+            clearInterval(currentDetectionIntervalId.current)
+            setDevice(device)
+            runPosenet()
+          }}>
+            {
+              devices.map(device => {
+                return <option key={device.deviceId}>{device.label}</option>
+              })
+            }
+          </select>
         </div>
       </header>
     </div>
