f59215904ff8f9be90c9a19b00f0f71acec08a1f
Saikat Mitra <saikatmitra91@gmail.com>
2022-11-13 17:17:46 +0530

    fix: add posnet similarity

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-// 1. Install dependencies DONE
-// 2. Import dependencies DONE
-// 3. Setup webcam and canvas DONE
-// 4. Define references to those DONE
-// 5. Load posenet DONE
-// 6. Detect function DONE
-// 7. Drawing utilities from tensorflow DONE
-// 8. Draw functions DONE
-
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
 import { drawKeypoints, drawSkeleton } from "./utilities";
+import { poseSimilarity } from 'posenet-similarity';
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const imgRef = useRef(null);
+  const posenetRef = useRef(false)
+  const [distance, setDistance] = useState(0);
+  const [maxDistance, setMaxDistance] = useState(distance)
 
   //  Load posenet
   const runPosenet = async () => {
+    posenetRef.current = true
     const net = await posenet.load({
       inputResolution: { width: 640, height: 480 },
       scale: 0.8,
     });
+    
     //
     setInterval(() => {
       detect(net);
     }, 100);
   };
 
+  /**
+   * @param {posenet.PoseNet} net 
+   */
   const detect = async (net) => {
     if (
       typeof webcamRef.current !== "undefined" &&
@@ -46,9 +47,12 @@ function App() {
       webcamRef.current.video.height = videoHeight;
 
       // Make Detections
+      const expectedPose = await net.estimateSinglePose(imgRef.current);
       const pose = await net.estimateSinglePose(video);
-      console.log(pose);
-
+      const data = poseSimilarity(expectedPose, pose, {
+        strategy: 'cosineSimilarity'
+      })
+      setDistance(data)
       drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
     }
   };
@@ -57,45 +61,57 @@ function App() {
     const ctx = canvas.current.getContext("2d");
     canvas.current.width = videoWidth;
     canvas.current.height = videoHeight;
-
     drawKeypoints(pose["keypoints"], 0.6, ctx);
     drawSkeleton(pose["keypoints"], 0.7, ctx);
   };
 
-  runPosenet();
+  useEffect(() => {
+    if (!posenetRef.current){
+      runPosenet();
+    }
+  })
+
+  useEffect( () => {
+    if(distance > maxDistance) {
+      setMaxDistance(distance)
+    }
+  }, [distance, maxDistance])
 
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
+          />
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
+          />
+        <div className="image-score">
+          <img ref={imgRef} crossOrigin="anonymous" src="https://ucarecdn.com/cae252d0-7c80-4523-834c-a0d3e053796f/-/preview/-/quality/smart/-/format/auto/" id="pose-match" style={{ width: "100%"}}/>
+          <h1>{maxDistance.toFixed(2)}</h1>
+        </div>
       </header>
     </div>
   );
